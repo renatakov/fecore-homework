cd208c60a8a4169d5b31d93771ba31b3d920ac33
renatakov <renatakov1010@gmail.com>
2022-10-08 12:51:45 +0200

    homework-4

---
+++ homework-4/js/task-1.js
@@ -0,0 +1,17 @@
+// 1. Створіть програму, яка виводить на екран 10 випадкових чисел із діапазону -25…+25.
+//  Після кожного від’ємного числа потрібно виводити (впритул до числа) символ «?».
+
+// Наприклад : 2, -8?,17,24,-17?,14
+let num1 = -25;
+let num2 = 25;
+
+let randomNumbers = parseInt(Math.random() * (num1 - num2) + num2)  ;
+
+
+
+if(randomNumbers < 0){
+console.log(randomNumbers + '?')
+} else{
+  console.log(randomNumbers);
+}
+
