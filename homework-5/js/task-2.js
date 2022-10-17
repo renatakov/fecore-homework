31d13a3e13f62f2719876be939337d49d8ac3c1b
renatakov <renatakov1010@gmail.com>
2022-10-17 19:10:13 +0200

    homework-5

---
+++ homework-5/js/task-2.js
@@ -0,0 +1,10 @@
+// 2. У нас є два масиви :
+// let arr1 = ['first','second'];
+// let arr2 = [3,4];
+// Об’єднайте їх в один масив.
+
+// let arr1 = ['first','second'];
+// let arr2 = [3,4];
+
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
---
+++ homework-5/js/task-3.js
@@ -0,0 +1,7 @@
+// 3. Є масив :
+// let arr = ['first','second','third'];
+// Видаліть в ньому перший елемент.
+
+// let arr = ['first','second','third'];
+// arr.shift();
+// console.log(arr)
