95c84fa5dd203d95b69a09c53ce1ffe21f3256a9
renatakov <renatakov1010@gmail.com>
2022-10-28 17:22:09 +0200

    homework-6 added

---
+++ homework-6/js/task-1.js
@@ -0,0 +1,11 @@
+// Напишіть функцію, яка приймає як аргумент відстань в кілометрах
+//  і повертає відстань в милях.
+
+
+
+// let km = +prompt('Enter the number of kilometrs: ');
+
+// function convert(km){
+//   return km * 0.6214;
+// }
+// alert(`Miles in ${km} kilometrs ${convert(km)}`);
---
+++ homework-6/js/task-2.js
@@ -0,0 +1,17 @@
+// Напишіть функцію, яка виводить квадрат ( наприклад, із зірочок ),
+// з розмірами, які залежать від глобальної змінної
+
+// ----------------
+
+// let side = +prompt("Enter the number of sides");
+// let str = "";
+// function square() {
+//   for (let i = 1; i <= side; i++) {
+//     for (let j = 1; j <= side; j++) {
+//       str += " * ";
+//     }
+//     console.log(str);
+//     str = "";
+//   }
+// }
+// square()
