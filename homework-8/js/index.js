71c9e655543dcbd1e943b433f39f4c2e17df642b
renatakov <renatakov1010@gmail.com>
2022-11-25 16:14:23 +0100

    homework-7 is done

---
+++ homework-8/js/index.js
@@ -0,0 +1,25 @@
+// Спробувати викликати div єлемент з
+// контентом за допомогою id та class.
+
+// const idDiv = document.getElementById('first-id');
+// const classDiv = document.getElementsByClassName('first-div')
+// console.log(idDiv);
+// console.log(classDiv);
+// --------------------
+
+// Створити текст у параграфі за допомогою js,
+// та спробувати брати текст з інпути та викликати
+//  це натисканням кнопки.
+// const btn = document.getElementById('first-btn');
+// const input = document.getElementById('input1')
+// const p = document.querySelector('.first-p')
+
+// p.innerHTML = 'Hello world!'
+
+
+// let btnFunction = () => {
+// let newTxt = input.value;
+// p.innerHTML = `<p>${newTxt}</p>`;
+// }
+
+// btn.onclick = btnFunction;
